11e98565802390eb553e6e5b7280164fc541a0cc
vicctorneves <vicctorneves88@gmail.com>
2022-06-03 12:40:10 -0300

    scroll jquery

--- js/script-scroll.js
+++ js/script-scroll.js
@@ -1,2 +1,10 @@
-const scroll = document.querySelector('#scroll');
-console.log(scroll);
+//jquery scroll-animate
+
+$('#btntoplanos').click(function(e){
+   e.preventDefault();
+   var id = $(this).attr('href');
+       targetOffSet = $(id).offset().top;
+   $('html, body').animate({
+      scrollTop: targetOffSet
+   },500);
+});
