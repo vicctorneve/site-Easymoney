35e12d45d680344fdfa8dc4878bfb82c23991f78
vicctorneves <vicctorneves88@gmail.com>
2022-06-06 17:40:43 -0300

    fade

--- js/script-scroll.js
+++ js/script-scroll.js
@@ -13,3 +13,24 @@ document.querySelector("#btntoplanos").addEventListener('click', function(event)
 
 // SCROLL SITE 
 
+const target = document.querySelectorAll('[data-anime]');
+const animationClass = 'animate';
+
+function animeScroll(){
+   const windowTop = window.pageYOffset + ((window.innerHeight*0.75));
+   target.forEach(function(element){
+      if((windowTop) > element.offsetTop) {
+         element.classList.add(animationClass);
+      } else{
+         element.classList.remove(animationClass);
+      }
+   })
+}
+
+animeScroll();
+
+if(target.length){
+   window.addEventListener('scroll', function() {
+      animeScroll();
+   })
+}
