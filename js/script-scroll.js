1ebaa701a2c9e5fb6935a6955914541ec60a7dc2
vicctorneves <vicctorneves88@gmail.com>
2022-06-08 20:30:22 -0300

    scrollbutton

--- js/script-scroll.js
+++ js/script-scroll.js
@@ -1,14 +1,11 @@
 // SCROLL BUTTON
-
 const element = document.body;
-
-
 function scrollTo(element) {
    document.querySelector(element).scrollIntoView({behavior: 'smooth'});
 }
-
 document.querySelector("#btntoplanos").addEventListener('click', function(event){
    event.preventDefault();
+   console.log('clicado')
    scrollTo("#planos");
 })
 
