cbfd517095f518e0bfbc295bb7990ba15f9daefa
vicctorneves <vicctorneves88@gmail.com>
2022-06-12 16:37:52 -0300

    menu

---
+++ js/scroll-menu.js
@@ -0,0 +1,24 @@
+const menuLinks = document.querySelectorAll('.menu a');
+
+menuLinks.forEach(item =>{
+   item.addEventListener('click', scrollToIdOnClick)
+})
+
+function getScrollTopByHref(element) {
+   const id = element.getAttribute('href');
+   return document.querySelector(id).offsetTop
+}
+
+function scrollToIdOnClick(event){
+   event.preventDefault();
+   const to = getScrollTopByHref(event.target);
+   scrollToPosition(to);
+}
+
+function scrollToPosition(to){
+   window.scroll({
+      top: to,
+      behavior: 'smooth'
+   });
+}
+
