cd59cc72e9b7f8976ccfb33b1b083b3d45995c88
vicctorneves <vicctorneves88@gmail.com>
2022-07-21 23:38:09 -0300

    menu-mobile

---
+++ js/script-menu-mobile.js
@@ -0,0 +1,16 @@
+const btnMobile = document.querySelector('.btn-menu');
+const listMenu = document.querySelectorAll('.menu li')
+
+
+
+btnMobile.addEventListener('click', toggleMenu);
+function toggleMenu(){
+   const nav = document.querySelector('nav');
+   nav.classList.toggle('active');
+}
+for (li of listMenu) {
+   li.addEventListener('click', closeMenu)
+   function closeMenu(){
+      toggleMenu()
+   }
+}
